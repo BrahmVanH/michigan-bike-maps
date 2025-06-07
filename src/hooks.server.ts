14e3150f61eb7e23aa14326f71ad9a8011cf586a
Brahm Van Houzen <brahmvanh@gmail.com>
2025-06-06 21:53:38 -0400

    added sha to headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -14,7 +14,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 
   response.headers.set(
     'Content-Security-Policy',
-    `default-src 'self'; script-src 'self' 'wasm-unsafe-eval' 'sha256-A0KtUzZjqbpeFRPxgjqKWTOxwZkt5Hj8I90vY6iPkLk=' 'sha256-DuGgxVdcpnS3W9cW3lYUmr9COOM7cGrDBaDyrTNauY8=' 'sha256-ArZS4bKt7vlTrUuJLICVouLGhK5bLkN3IPuCEHF9Ry0=' 'sha256-ZdcyT9UHm2RBYyGqChEN+cEuscyc3zkCR7i1APqE6g8=' 'sha256-kbtFhEcdr0jFD6Mza8SI5Qqj3pBq4XOVTZwWqY6FDnM=' 'sha256-CmBOr6DT+IJnxskHTVK4bLDpD9ifMThrsDuOSuhl4lY=' 'nonce-${nonce}'; style-src 'self' 'unsafe-inline'; connect-src 'self' blob:; img-src 'self' data:; worker-src 'self' blob:;`
+    `default-src 'self'; script-src 'self' 'wasm-unsafe-eval' 'sha256-A0KtUzZjqbpeFRPxgjqKWTOxwZkt5Hj8I90vY6iPkLk=' 'sha256-DuGgxVdcpnS3W9cW3lYUmr9COOM7cGrDBaDyrTNauY8=' 'sha256-ArZS4bKt7vlTrUuJLICVouLGhK5bLkN3IPuCEHF9Ry0=' 'sha256-ZdcyT9UHm2RBYyGqChEN+cEuscyc3zkCR7i1APqE6g8=' 'sha256-kbtFhEcdr0jFD6Mza8SI5Qqj3pBq4XOVTZwWqY6FDnM=' 'sha256-CmBOr6DT+IJnxskHTVK4bLDpD9ifMThrsDuOSuhl4lY=' 'sha256-CqfYMUIFL74fhQmBN6Xf/7FmcOjZZBPj71joiZ3skNo=' 'nonce-${nonce}'; style-src 'self' 'unsafe-inline'; connect-src 'self' blob:; img-src 'self' data:; worker-src 'self' blob:;`
   );
 
   response.headers.set('X-Content-Type-Options', 'nosniff');
